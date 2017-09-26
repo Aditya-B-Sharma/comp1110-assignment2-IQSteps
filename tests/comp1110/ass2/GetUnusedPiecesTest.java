f384016fa47cb899da75bd4a89212cf19e2ac2d9
Khamis Buol <u6080028@anu.edu.au>
2017-09-26 20:10:25 +1000

    Originality updated and tests done for getUnusedPieces

--- tests/comp1110/ass2/GetUnusedPiecesTest.java
+++ tests/comp1110/ass2/GetUnusedPiecesTest.java
@@ -11,22 +11,33 @@ import static junit.framework.TestCase.assertTrue;
  * Created by Khamis on 24/09/2017.
  */
 public class GetUnusedPiecesTest {
-    String objective = "DBgGAiFCNBGKCFlAFnHHSECP";
-    String placement = "BGKFCNCFl";
-    String unused = "DBgGAiAFnHHSECP";
 
-    String[] ss = {"ABNJSHFSF","AKFSBFW","HDJGJHE","FJHHFHS","OPKFPSFJ"};
+    /*Empty placement but non-empty objective*/
+    String obj = "GHs";
+    String emptPlace = "";
+    String unused3 = "GHs";
 
     @Test
     public void testGood(){
-        assertTrue(Collections.singletonList(unused).equals(Collections.singletonList(StepsGame.getUnusedPieces(placement, objective))));
+        String objective = "DBgGAiFCNBGKCFlAFnHHSECP";
+        String placement = "BGKFCNCFl";
+        String unused = "DBgGAiAFnHHSECP";
+        assertTrue("Expected " + unused + " but got: " + StepsGame.getUnusedPieces(placement, objective) ,Collections.singletonList(unused).equals(Collections.singletonList(StepsGame.getUnusedPieces(placement, objective))));
+        assertTrue("Expected 0, but got: " + unused.length()%3, unused.length()%3 == 0);
     }
-    @Test
-    public void testSize() {
-        assertTrue(unused.length() == StepsGame.getUnusedPieces(placement, objective).length());
-    }
-    @Test
-    public void testBad() {
 
+    @Test
+    public void testNumberOfUnusedPieces() {
+        String objective = "DBgGAiFCNBGKCFlAFnHHSECP";
+        String placement = "BGKFCNCFl";
+        String unused = "DBgGAiAFnHHSECP";
+        //assertTrue(unused.length() == StepsGame.getUnusedPieces(placement, objective).length());
+        assertTrue("For objective, "+ objective + " and " +" placement, " + placement + ", number of unused pieces should be " + unused.length()/3 + " but got " + StepsGame.getUnusedPieces(placement, objective).length()/3, unused.length() == StepsGame.getUnusedPieces(placement, objective).length());
     }
+
+    @Test
+    public void testEmptyPlacement() {
+        assertTrue("For empty placement, expected " + unused3 + " but got: " + StepsGame.getUnusedPieces(emptPlace, obj), unused3.equals(StepsGame.getUnusedPieces(emptPlace, obj)));
+    }
+
 }
