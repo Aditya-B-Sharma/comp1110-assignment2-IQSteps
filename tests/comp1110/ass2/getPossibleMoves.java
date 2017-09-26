b6f292a249974ca28063e1e4f32da65cf281126f
Stefan Louie <u6051635@uds.anu.edu.au>
2017-09-26 12:42:45 +1000

    Working on JUnit Tests for getPossibleMoves and updateRemainingMoves.

---
+++ tests/comp1110/ass2/getPossibleMoves.java
@@ -0,0 +1,26 @@
+package comp1110.ass2;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Stefan on 26/09/2017.
+ */
+public class getPossibleMoves {
+
+    String[] testSimple = {"AA"};
+    String[] locations = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y",
+            "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"};
+    String[] testSimpleMoves = {"AAL","AAN","AAP","AAR","AAW","AAY",};
+
+    @Test
+    public void testSimple() {
+        ArrayList<String> testSimpleMovesL = new ArrayList<>(Arrays.asList(testSimpleMoves));
+        assertTrue("Failed to get all possible moves, or has an invalid piece placement.", Arrays.equals(testSimpleMovesL.toArray(),testSimpleMoves));
+    }
+
+}
